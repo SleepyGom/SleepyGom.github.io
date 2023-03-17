46531731f947ca16b362156499482a525f9d7195
정승일 <jungsi1217@naver.com>
2023-03-17 22:44:02 +0900

    company create

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path='/' element={<App/>}/>
       <Route path='/company' element={<Company/>}/>
